2ac57ef1bcd102e5b47e566b24e16f05b965cc0f
SantosSjba <bernuijesus363@gmail.com>
2026-05-31 00:11:57 -0500

    Confi Envs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      envFilePath: [
+        `.env.${process.env.NODE_ENV}`,
+        '.env',
+      ]
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
