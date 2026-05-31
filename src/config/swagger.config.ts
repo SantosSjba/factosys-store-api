5b87a67eef3fcbf6bf433d44213cdc6571484594
SantosSjba <bernuijesus363@gmail.com>
2026-05-31 00:47:30 -0500

    Swagger configurado

---
+++ src/config/swagger.config.ts
@@ -0,0 +1,24 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export function setupSwagger(app: INestApplication): void {
+  const config = new DocumentBuilder()
+    .setTitle('Factosys Store API')
+    .setDescription(
+      'API oficial de Factosys Store para administración y e-comerce',
+    )
+    .setVersion('1.0.0')
+    .addBearerAuth({
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+    })
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
+}
